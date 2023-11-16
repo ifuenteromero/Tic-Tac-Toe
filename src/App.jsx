519de78a5fd811f469cc10d799ed4f718a9eb33f
Irene Fuente Romero <ifuenteromero@gmail.com>
2023-11-16 19:41:45 +0100

    Initail styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,18 @@ import "./css/App.css";
 
 const App = () => {
 	return (
-		<>
-			<h1>Tic Tac Toe</h1>
-		</>
+		<div className="page">
+			<header>
+				<h1>Tic Tac Toe</h1>
+			</header>
+			<main>
+				<section className="turn">Turn X</section>
+				<section className="game">
+					<div className="board"></div>
+					<button className="reset-button">Restar game</button>
+				</section>
+			</main>
+		</div>
 	);
 };
 
