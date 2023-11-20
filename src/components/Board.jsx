c51a4fd3f7267e98b8f51f293f46d850cf7de7c2
Irene Fuente Romero <ifuenteromero@gmail.com>
2023-11-20 19:44:10 +0100

    si ya está la casilla rellena no permite reescribirla

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,5 +1,9 @@
 const Board = ({ updateBoard, currentTurn, matrix }) => {
+	const isAlreadyFilled = (matrix, rowIndex, colIndex) =>
+		matrix[rowIndex][colIndex] !== null;
+
 	const handleClick = (rowIndex, colIndex, currentTurn) => {
+		if (isAlreadyFilled(matrix, rowIndex, colIndex)) return;
 		updateBoard(rowIndex, colIndex, currentTurn);
 	};
 
@@ -12,10 +16,16 @@ const Board = ({ updateBoard, currentTurn, matrix }) => {
 						<tr key={rowIndex} className="board__row">
 							{row.map((col, colArrayIndex) => {
 								const colIndex = colArrayIndex + 1;
+								const isFilled = isAlreadyFilled(
+									matrix,
+									rowArrayIndex,
+									colArrayIndex
+								);
 								return (
 									<td
 										key={colIndex}
 										className="board__cell"
+										data-filled={isFilled}
 										onClick={() =>
 											handleClick(
 												rowArrayIndex,
